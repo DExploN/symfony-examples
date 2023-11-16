095efaeef255d5b89224b09d3b7505b9d88effc1
Pavel Golov <golov@softinvent.ru>
2023-11-16 17:38:26 +0300

    Symfony 5.4.31 init

---
+++ app/src/Messenger/Handler.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Messenger;
+
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class Handler implements MessageHandlerInterface
+{
+    public function __invoke(Message $message)
+    {
+        dd($message);
+    }
+}
